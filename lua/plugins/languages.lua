ec96ca49aa3dde077196a7a7336449dd31e39f3a
Przemek <przempore@gmail.com>
2026-02-22 22:04:51 +0100

    [obsidian] remove daily_notes configuration

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -33,30 +33,31 @@ return {
     },
     config = function()
       local obsidian_path = vim.fn.expand("~/Projects/second-brain")
-      if vim.fn.isdirectory(obsidian_path) == 1 then
-        local daily_notes_path = require("my_config.date_formatter_for_obsidian").get_formatted_path()
-        local daily_notes_day = require("my_config.date_formatter_for_obsidian").get_filename()
-
-        require("obsidian").setup({
-          workspaces = {
-            {
-              name = "second-brain",
-              path = "~/Projects/second-brain",
-            },
-          },
-          follow_url_func = function(url)
-            vim.ui.open(url)
-          end,
-          -- templates = {
-          --   folder = "~/Projects/second-brain/extras/templates/daily_note_template",
-          -- },
-          daily_notes = {
-            folder = string.format("Timestamps/%s", daily_notes_path),
-            date_format = string.format("%s", daily_notes_day),
-            template = nil,
-          },
-        })
+      if vim.fn.isdirectory(obsidian_path) == 0 then
+        return
       end
+      -- local daily_notes_path = require("my_config.date_formatter_for_obsidian").get_formatted_path()
+      -- local daily_notes_day = require("my_config.date_formatter_for_obsidian").get_filename()
+
+      require("obsidian").setup({
+        workspaces = {
+          {
+            name = "second-brain",
+            path = "~/Projects/second-brain",
+          },
+        },
+        follow_url_func = function(url)
+          vim.ui.open(url)
+        end,
+        -- templates = {
+        --   folder = "~/Projects/second-brain/extras/templates/daily_note_template",
+        -- },
+        -- daily_notes = {
+        --   folder = string.format("Timestamps/%s", daily_notes_path),
+        --   date_format = string.format("%s", daily_notes_day),
+        --   template = nil,
+        -- },
+      })
     end,
   },
 
